4e9a7c6fde677edea5d1509d93753e0915434b1a
nxhu64 <nxhu64@gmail.com>
2021-04-04 10:41:53 -0300

    Update EntityFunctions.cpp

--- TDU/EntityFunctions.cpp
+++ TDU/EntityFunctions.cpp
@@ -23,7 +23,7 @@ tVoxFunction InitializeVox;
 
 Vox* Teardown::Functions::EntityFunctions::LoadVox(Teardown::small_string* voxPath, const char* object, float Scale)
 {
-	Teardown::small_string objss(object);
+	Teardown::small_string objss;
 
 	Vox* newVox = tdLoadVox(voxPath, &objss, Scale);
 
