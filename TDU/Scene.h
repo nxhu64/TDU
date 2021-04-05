d00c1f80a21012c46dc258a9321770f585507a1b
nxhu64 <nxhu64@gmail.com>
2021-04-05 19:23:27 -0300

    Added Skybox, fixed Water ptr

--- TDU/Scene.h
+++ TDU/Scene.h
@@ -2,6 +2,7 @@
 #include "Types.h"
 #include "Entities.h"
 #include "Script.h"
+#include "Skybox.h"
 
 class Scene
 {
@@ -12,7 +13,10 @@ public:
 	void* pParticleSystem; //0x0070
 	char pad_0078[48]; //0x0078
 	class fireSystem* pFireSystem; //0x00A8
-	char pad_00B0[112]; //0x00B0
+	char pad_00B0[16]; //0x00B0
+	Skybox* pSkybox; //0x00C0
+	glm::vec3 spawnPos; //0x00C8
+	char pad_00D4[76]; //0x00D4
 	class Vehicle* pCurrentVehicle; //0x0120
 	class Screen* pCurrentScreen; //0x0128
 	char pad_0130[248]; //0x0130
@@ -21,11 +25,11 @@ public:
 	Teardown::small_vector<Shape*>* pShapeList; //0x0240
 	Teardown::small_vector<void*>* pLightList; //0x0248
 	Teardown::small_vector<void*>* pLocationList; //0x0250
-	Teardown::small_vector<void*>* pWaterList; //0x0258
+	Teardown::small_vector<Water*>* pWaterList; //0x0258
 	Teardown::small_vector<void*>* pEnemyList; //0x0260
 	Teardown::small_vector<void*>* pJointList; //0x0268
 	Teardown::small_vector<Vehicle*>* pVehicleList; //0x0270
-	Teardown::small_vector<void*>* pWheelList; //0x0278
+	Teardown::small_vector<Wheel*>* pWheelList; //0x0278
 	Teardown::small_vector<void*>* pScreenList; //0x0280
 	Teardown::small_vector<void*>* pTriggerList; //0x0288
 	Teardown::small_vector<Script*>* pScriptList; //0x0290
