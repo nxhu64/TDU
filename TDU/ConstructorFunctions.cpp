a9f81aecbccde26ed37bacf5a466971c4186f69d
nxhu64 <nxhu64@gmail.com>
2021-04-08 04:23:23 -0300

    added NewLight

--- TDU/ConstructorFunctions.cpp
+++ TDU/ConstructorFunctions.cpp
@@ -26,12 +26,6 @@ tEntityCCtor ScreenCCtor;
 tEntityCCtor TriggerCCtor;
 tEntityCCtor ScriptCCtor;
 
-Script* Teardown::Functions::Constructors::newScript(Entity* Parent)
-{
-	void* memAlloc = Teardown::Mem::Alloc(sizeof(Script));
-	return (Script*)ScriptCCtor(memAlloc, Parent);
-}
-
 Body* Teardown::Functions::Constructors::newBody(Entity* Parent)
 {
 	void* memAlloc = Teardown::Mem::Alloc(sizeof(Body));
@@ -42,6 +36,18 @@ Shape* Teardown::Functions::Constructors::newShape(Entity* Parent)
 {
 	void* memAlloc = Teardown::Mem::Alloc(sizeof(Shape));
 	return (Shape*)ShapeCCtor(memAlloc, Parent);
+}
+
+Light* Teardown::Functions::Constructors::newLight(Entity* Parent)
+{
+	void* memAlloc = Teardown::Mem::Alloc(sizeof(Light));
+	return (Light*)LightCCtor(memAlloc, Parent);
+}
+
+Script* Teardown::Functions::Constructors::newScript(Entity* Parent)
+{
+	void* memAlloc = Teardown::Mem::Alloc(sizeof(Script));
+	return (Script*)ScriptCCtor(memAlloc, Parent);
 }
 
 void Teardown::Functions::Constructors::GetAddresses()
@@ -55,7 +61,11 @@ void Teardown::Functions::Constructors::GetAddresses()
 	DWORD64 dwScriptCCtor = Memory::FindPattern(Signatures::ClassConstructors::Script.pattern, Signatures::ClassConstructors::Script.mask, Globals::HModule);
 	ScriptCCtor = (tEntityCCtor)Memory::readPtr(dwScriptCCtor, 1);
 
+	DWORD64 dwLightCCtor = Memory::FindPattern(Signatures::ClassConstructors::Light.pattern, Signatures::ClassConstructors::Light.mask, Globals::HModule);
+	LightCCtor = (tEntityCCtor)Memory::readPtr(dwScriptCCtor, 1);
+
 	WriteLog(LogType::Address, "Body::Body: 0x%p", BodyCCtor);
 	WriteLog(LogType::Address, "Shape::Shape: 0x%p", ShapeCCtor);
+	WriteLog(LogType::Address, "Light::Light: 0x%p", LightCCtor);
 	WriteLog(LogType::Address, "Script::Script: 0x%p", ScriptCCtor);
 }
